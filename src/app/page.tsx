cf53c431b580e76f4c9f4f5b0855d3e2888392c9
jukalbf <lgbr212@gmail.com>
2026-03-08 00:01:39 -0300

    style: Removendo 'justify-content: center' e importando Main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { FaGear } from "react-icons/fa6";
+import Main from "@/components/main";
+import StatusProvider from "@/providers/status.providers";
 
 export default function Home() {
   return (
     <main className={styles.page}>
-      <h1>
-        Já iniciaremos os trabalhos... <FaGear />
-      </h1>
+      <StatusProvider>
+        <Main />
+      </StatusProvider>
     </main>
   );
 }
